ac34284d9e179724402538938b43b5c90f8e22ac
k19ashok <101946227+k19ashok@users.noreply.github.com>
2022-06-17 12:19:45 +0530

    Students
    sdfw

--- src/main/java/com/example/demo/students2/studentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.students2;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface studentsRepository extends MongoRepository <students, String>{
-
-}

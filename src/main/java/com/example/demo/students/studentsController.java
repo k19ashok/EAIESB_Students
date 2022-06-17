660ea217e46f8d5aaf1f1832d3ec2dc0c967d182
k19ashok <101946227+k19ashok@users.noreply.github.com>
2022-06-17 12:24:27 +0530

    Update studentsController.java

--- src/main/java/com/example/demo/students/studentsController.java
+++ src/main/java/com/example/demo/students/studentsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eaiesb.training.students.studentsRepository;
+import com.example.demo.students.studentsRepository;
 
 
 
